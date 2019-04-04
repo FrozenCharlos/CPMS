ee5fd81d94731913d3b2bd1681860f66b0c0fe9d
jinweijie <jinweijie@neusoft.com>
2019-04-04 17:09:58 +0800

    add WebRoot

--- WebRoot/site/js/layuibase/js/index.js
+++ WebRoot/site/js/layuibase/js/index.js
@@ -1,21 +1,23 @@
 //index首页配置文件
 layui.config({//全局配置
-	base: '/site/js/layuibase/js/'
-}).use(['element', 'layer', 'navbar', 'tab','form'], function() {//加载
+	base: 'site/js/layuibase/plugins/layui/lay/modules/'
+}).use(['element', 'layer', 'navbar', 'tab','form','table'], function() {//加载
 	var element = layui.element(),
 		$ = layui.jquery,
 		layer = layui.layer,
 		form = layui.form,
 		navbar = layui.navbar(),
-		tab = layui.tab({
-			elem: '.admin-nav-card' //设置选项卡容器
-		});
-	//设置navbar导航栏
-	navbar.set({
-		spreadOne: true,
-		elem: '#admin-navbar-side',
-		cached: true,
-		data: navss
+    	table = layui.table,
+        tab = layui.tab({
+            elem: '.admin-tab' //设置选项卡容器
+        });
+    //渲染navbar
+    navbar.render();
+    //监听点击事件
+    navbar.on('click(test)', function() {
 
-	});
+
+    layer.alert("gg");
 });
+});
+
