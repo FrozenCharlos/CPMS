601463e23dd3fad3ae689b55cf539b8cdd5e64d4
jinweijie <jinweijie@neusoft.com>
2019-04-10 16:27:45 +0800

    add target

--- target/classes/site/roomManage.sql
+++ target/classes/site/roomManage.sql
@@ -1,9 +1,10 @@
 #sql("getRoomInfoById")
-select r.room_id,r.area from room_info r,building_info b where r.room_id = ? and b.building_id = r.building_id and b.building_name = ?
+select r.room_id,r.area,b.building_name,u.user_name,u.user_idnumber from room_info r,building_info b,user_info u where r.room_id = ? and b.building_id = r.building_id and u.user_id = r.owner_id
 #end
 #sql("getRoomInfoByOwner")
 select r.room_id,r.area,b.building_name from room_info r,building_info b where  and b.building_id = r.building_id and r.owner_id = ?
 #end
 #sql("getAllRoomInfo")
-select b.building_name,r.room_id,r.area from room_info r,building_info b where  b.building_id = r.building_id
+select r.room_id,r.area,b.building_name,u.user_name,u.user_idnumber from building_info b,room_info r LEFT JOIN user_info u on   u.user_id = r.owner_id where b.building_id = r.building_id
 #end
+
